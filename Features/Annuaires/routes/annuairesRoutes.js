0839eee2a2d46a01ec91a38abe4f9ddef30f7db2
Mechichi Rimeh <rimehmechichi08@gmail.com>
2025-06-04 17:27:19 +0100

    adding just the get for the annuaires

--- Features/Annuaires/routes/annuairesRoutes.js
+++ Features/Annuaires/routes/annuairesRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const annuaireController = require('../views/annuairesController.js');
 
-router.post('/annuaire', annuaireController.create);
 router.get('/annuaire', annuaireController.getAll);
 router.get('/annuaire/:id', annuaireController.getById);
-router.put('/annuaire/:id', annuaireController.update);
-router.delete('/annuaire/:id', annuaireController.delete);
 
 module.exports = router;
--- Features/Annuaires/viewModels/annuairesService.js
+++ Features/Annuaires/viewModels/annuairesService.js
@@ -1,10 +1,5 @@
 const annuaire = require('../models/annuairesModels');
 
-// Create
-async function createannuaire(data) {
-  return await annuaire.create(data);
-}
-
 // Read All
 async function getAllannuaires() {
   return await annuaire.find();
@@ -15,20 +10,8 @@ async function getannuaireById(idannuaire) {
   return await annuaire.findOne({ idannuaire });
 }
 
-// Update
-async function updateannuaire(idannuaire, data) {
-  return await annuaire.findOneAndUpdate({ idannuaire }, data, { new: true });
-}
-
-// Delete
-async function deleteannuaire(idannuaire) {
-  return await annuaire.findOneAndDelete({ idannuaire });
-}
 
 module.exports = {
-  createannuaire,
   getAllannuaires,
   getannuaireById,
-  updateannuaire,
-  deleteannuaire,
 };
