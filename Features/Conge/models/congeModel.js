99f1d57f1434ab5427c67f7b036ba0abb0eee861
Mechichi Rimeh <rimehmechichi08@gmail.com>
2025-07-17 14:51:57 +0100

    fixing the add conge and update structur

--- Features/Conge/models/congeModel.js
+++ Features/Conge/models/congeModel.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 const congeSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  dateDebut: { type: Date, required: false },
-  dateFin: { type: Date, required: false },
+  dateDebut: { type: Date, required: true },
+  dateFin: { type: Date, required: true },
   type: { type: String },
-  statut: { type: String },
+  statut: {
+    type: String,
+    enum: ['Approuvé', 'Refusé', 'En attente'],
+    default: 'En attente'
+  },
   soldeRestant: { type: Number, required: true, default: 25 },
   certificat: { type: String },
   commentaire: { type: String },
