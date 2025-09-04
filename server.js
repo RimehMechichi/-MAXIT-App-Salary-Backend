13cbad66ba22ffe117a125947a82234350e857b7
Mechichi Rimeh <rimehmechichi08@gmail.com>
2025-09-04 22:59:30 +0100

    fixing the user put

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 8080;
 const path = require('path');
 
 app.use('/images', express.static(path.join(__dirname, 'Features/Ecologiques/Public/images')));
-app.use('/images/auth', express.static(path.join(__dirname, 'Features/Authentification/Public/images')));
+app.use('/images', express.static(path.join(__dirname, 'Features/Authentification/Public/images')));
 
 app.use(cors());
 app.use(morgan("dev"));
